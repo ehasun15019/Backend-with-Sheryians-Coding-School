db7a44ff7dc5bff812bf52e17d67f5cfc354e941
Your Full Name <ehasun0909@gamil.com>
2025-09-14 21:52:34 +0600

    learn lec4

---
+++ lec4-express-routing-middleware/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+// middleware
+app.use(function (req, res, next) {
+  console.log("This is a middleware ");
+
+  next();
+});
+
+// Custom error-handling middleware
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ message: "Oops! Something went wrong." });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
