f88a84fb4e4c7766c23b013e3607a5cd1a172fb8
ehasun15019 <ehasunulislam0202@gmail.com>
2025-09-20 00:27:47 +0600

    update lec-9

--- lec9-pp-3/index.js
+++ lec9-pp-3/index.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 3000;
 const path = require("path");
 const fs = require("fs");
+const { log } = require("console");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,6 +23,18 @@ app.get("/", (req, res) => {
 app.get("/file/:filename", (req, res) => {
   fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err, filedata) {
    res.render("show", { filename: req.params.filename, filedata: filedata });
+  })
+});
+
+// for edit page
+app.get("/edit/:filename", (req, res) => {
+  res.render("edit", { filename: req.params.filename });
+})
+
+// for editing task management
+app.post("/edit", (req, res) => {
+  fs.rename(`./files/${req.body.previousFilename}`, `./files/${req.body.newFilename}.txt`, function(err) {
+    res.redirect("/");
   })
 });
 
